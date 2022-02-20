5be3d4c630315f1e1181e917c3154193fd49b510
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2022-02-21 01:02:16 +0900

    Time: 73 ms (87.94%), Space: 42.9 MB (27.63%) - LeetHub

---
+++ 859-buddy-strings/859-buddy-strings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+  if(s.length !== goal.length) return false;
+  if(s === goal){
+ 		return [...s].some((sWord, sIdx) => {
+      console.log(goal.lastIndexOf(sWord), sWord)
+      return goal.lastIndexOf(sWord) !== sIdx;
+    })   
+  } else {
+    const _goal = [];
+    const _s = [...s].reduce((sList, sWord, idx) => {
+      if(sWord !== goal[idx]) {
+        _goal.push(goal[idx]) 
+        sList.push(sWord)
+      }
+      return sList;
+    }, [])
+    return _goal.length === 2 && _goal[0] === _s[1] && _goal[1] === _s[0];
+
+  }
+}
