fe4ac0739c4add05d66babdd0a4cd06353a590e0
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2022-02-21 01:14:29 +0900

    Time: 105 ms (49.03%), Space: 42.7 MB (33.07%) - LeetHub

--- 859-buddy-strings/859-buddy-strings.js
+++ 859-buddy-strings/859-buddy-strings.js
@@ -10,6 +10,13 @@ const isSameElementWhenSwitched = ([targetFirst, targetLast], [goalFist, goalLas
     return targetFirst === goalLast && targetLast === goalFist
 }
 
+/*
+* 글자가 같으면: 선택된 글자가 다른 index에 있는지 확인.
+* 글자가 다르면: 
+** 1. list diff만 추출
+** 2. 횟수제한 validation
+** 3. switch했을때 같은 element인지
+*/
 var buddyStrings = function(s, goal) {
     if(s.length !== goal.length) return false;
     if(s === goal) return [...s].some((sWord, sIdx) =>  goal.lastIndexOf(sWord) !== sIdx);
