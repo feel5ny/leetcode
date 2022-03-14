bc2b8972b40931b220d548d6d2149d3a32d6e569
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2022-03-15 00:06:05 +0900

    Time: 77 ms (60.16%), Space: 41.9 MB (57.57%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  	const result = [[1]];
+  
+    const recursive = (rowIndex, preRow) => {
+      if(rowIndex === numRows) return;
+        
+      const draftPreRow = [...preRow];
+      draftPreRow.push(0);
+      draftPreRow.unshift(0);
+      
+      const curRow = draftPreRow.map((num, idx) => num + draftPreRow[idx + 1])
+      curRow.pop();
+      result.push(curRow);
+        
+      recursive(rowIndex + 1, curRow)
+    }
+    
+    recursive(1, [1]);
+  	return result;
+};
